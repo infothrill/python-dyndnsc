37a6dc361db423b72d7c0ecf6c3757bef726bf48
Paul Kremer <pkremer@spurious.biz>
2013-11-01 16:06:55 +0100

    warn when overwriting already registered observers

--- dyndnsc/common/subject.py
+++ dyndnsc/common/subject.py
@@ -5,6 +5,8 @@ observer/subject implementation for desktop notification
 
 import logging
 
+log = logging.getLogger(__name__)
+
 
 class Subject(object):
     """
@@ -25,18 +27,21 @@ class Subject(object):
         if events is not None and type(events) not in (tuple, list):
             events = (events,)
 
+        if observer in self._observers:
+            log.warn("Observer '%r' already registered, overwriting for events %r", observer, events)
         self._observers[observer] = events
 
     def notify_observers(self, event=None, msg=None):
         """notify observers """
         for observer, events in list(self._observers.items()):
+            #log.debug("trying to notify the observer")
             if events is None or event is None or event in events:
                 try:
                     observer(self, event, msg)
                 except (Exception,) as ex:  # pylint: disable=W0703
                     self.unregister_observer(observer)
                     errmsg = "Exception in message dispatch: Handler '{0}' unregistered for event '{1}'  ".format(observer.__class__.__name__, event)
-                    logging.error(errmsg, exc_info=ex)
+                    log.error(errmsg, exc_info=ex)
 
     def unregister_observer(self, observer):
         """ unregister observer function """
