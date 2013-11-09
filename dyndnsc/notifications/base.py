08301a365bb2c59e6c1a7ef461895cda4c2000fe
Paul Kremer <pkremer@spurious.biz>
2013-11-09 09:53:20 +0100

    remove unused methods

--- dyndnsc/notifications/base.py
+++ dyndnsc/notifications/base.py
@@ -10,19 +10,11 @@ class Notification(object):
 
     default_title = "Dynamic DNS"
 
-    def is_disabled(self):
-        return not self.is_enabled()
-
-    def is_enabled(self):
-        return True  # TODO
-
     def create_notify_handler(self):
         return self._notify
 
     def _notify(self, *args, **kwargs):
-        if self.is_enabled():
-            return self.notify(*args, **kwargs)
-        return False
+        return self.notify(*args, **kwargs)
 
     @abstractmethod
     def notify(self, caller, event, message):
