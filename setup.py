e9580d353ac69b8bbba90826ab4d72d23a2a5809
Paul Kremer <pkremer@spurious.biz>
2013-11-09 09:57:46 +0100

    require requests. Fixes #1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
 """.splitlines() if len(line) > 0]
 
-install_requires = ["IPy>=0.56"]
+install_requires = ["requests", "IPy>=0.56"]
 if sys.version_info >= (3, 0):
     install_requires.append("netifaces-py3==0.8")
 else:
@@ -38,6 +38,8 @@ else:
 
 if sys.version_info < (3, 2):
     install_requires.append("argparse")
+#if sys.version_info < (2, 7):  # continue support for python 2.6
+#    install_requires.append("importlib")
 
 setup(name="dyndnsc",
       packages=["dyndnsc", "dyndnsc.common", "dyndnsc.detector", "dyndnsc.updater", "dyndnsc.tests"],
@@ -55,7 +57,5 @@ setup(name="dyndnsc",
                       """),
       classifiers=classifiers,
       test_suite='dyndnsc.tests',
-      #test_suite='nose.collector',
-      #setup_requires=['nose'],
       tests_require=['bottle==0.11.6'],
       )
