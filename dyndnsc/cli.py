46e5337b2cc7b3c19a151ed3f701cb2b3444a63a
Paul Kremer <pkremer@spurious.biz>
2013-11-13 13:31:19 +0100

    line lengths

--- dyndnsc/cli.py
+++ dyndnsc/cli.py
@@ -15,17 +15,35 @@ from dyndnsc.daemon import daemonize
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--daemon", dest="daemon", help="go into daemon mode (implies --loop)", action="store_true", default=False)
-    parser.add_argument("--debug", dest="debug", help="increase logging level to DEBUG", action="store_true", default=False)
-    parser.add_argument("--hostname", dest="hostname", help="hostname to update", default=None)
-    parser.add_argument("--key", dest="key", help="your authentication key", default=None)
-    parser.add_argument("--userid", dest="userid", help="your userid", default=None)
-    parser.add_argument("--password", dest="password", help="your password", default=None)
-    parser.add_argument("--protocol", dest="protocol", help="protocol/service to use for updating your IP (default dyndns)", default='dyndns')
-    parser.add_argument("--method", dest="method", help="method for detecting your IP (default webcheck)", default='webcheck')
-    parser.add_argument("--loop", dest="loop", help="loop forever (default is to update once)", action="store_true", default=False)
-    parser.add_argument("--sleeptime", dest="sleeptime", help="how long to sleep between checks in seconds", default=300)
-    parser.add_argument("--version", dest="version", help="show version and exit", action="store_true", default=False)
+    parser.add_argument("-d", "--daemon", dest="daemon",
+                        help="go into daemon mode (implies --loop)",
+                        action="store_true", default=False)
+    parser.add_argument("--debug", dest="debug",
+                        help="increase logging level to DEBUG",
+                        action="store_true", default=False)
+    parser.add_argument("--hostname", dest="hostname",
+                        help="hostname to update", default=None)
+    parser.add_argument("--key", dest="key",
+                        help="your authentication key", default=None)
+    parser.add_argument("--userid", dest="userid",
+                        help="your userid", default=None)
+    parser.add_argument("--password", dest="password",
+                        help="your password", default=None)
+    parser.add_argument("--protocol", dest="protocol",
+                        help="protocol to use for updating IP (default dyndns)",
+                        default='dyndns')
+    parser.add_argument("--method", dest="method",
+                        help="method for detecting your IP (default webcheck)",
+                        default='webcheck')
+    parser.add_argument("--loop", dest="loop",
+                        help="loop forever (default is to update once)",
+                        action="store_true", default=False)
+    parser.add_argument("--sleeptime", dest="sleeptime",
+                        help="how long to sleep between checks in seconds",
+                        default=300)
+    parser.add_argument("--version", dest="version",
+                        help="show version and exit",
+                        action="store_true", default=False)
     args = parser.parse_args()
 
     if args.version:
@@ -37,7 +55,8 @@ def main():
     else:
         level = logging.INFO
 
-    logging.basicConfig(level=level, format='%(asctime)s %(levelname)s %(message)s')
+    logging.basicConfig(level=level,
+                        format='%(asctime)s %(levelname)s %(message)s')
     # silence 'requests' logging
     requests_log = logging.getLogger("requests")
     requests_log.setLevel(logging.WARNING)
