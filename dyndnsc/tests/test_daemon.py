846667731c2c3835e73a3570ef1635e9f59b5f9c
Paul Kremer <pkremer@spurious.biz>
2013-12-17 08:44:17 +0100

    test skeleton

---
+++ dyndnsc/tests/test_daemon.py
@@ -0,0 +1,16 @@
+import unittest
+
+from dyndnsc import daemon
+
+
+class TestDaemon(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_daemonize(self):
+        # TODO
+        pass
