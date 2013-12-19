99e8b5fe9b549afacb2b15cdaf19ee84b80af511
Paul Kremer <pkremer@spurious.biz>
2013-12-19 16:31:00 +0100

    get rid of currently useless junk methods and state

--- dyndnsc/updater/base.py
+++ dyndnsc/updater/base.py
@@ -18,7 +18,6 @@ class UpdateProtocol(Subject):
     _updateurl = None
     theip = None
     hostname = None  # this holds the desired dns hostname
-    status = 0
 
     def __init__(self):
         self.updateurl = self._updateurl
@@ -26,29 +25,6 @@ class UpdateProtocol(Subject):
 
     def updateUrl(self):
         return self.updateurl
-
-    def success(self):
-        self.status = 0
-        self.notify_observers(IP_UPDATE_SUCCESS, "Updated IP address of '%s' to %s" % (self.hostname, self.theip))
-
-    def abuse(self):
-        self.status = 1
-        self.notify_observers(IP_UPDATE_ERROR, "This client is considered to be abusive for hostname '%s'" % (self.hostname))
-
-    def nochg(self):
-        self.status = 0
-
-    def nohost(self):
-        self.status = 1
-        self.notify_observers(IP_UPDATE_ERROR, "Invalid/non-existant hostname: [%s]" % (self.hostname))
-
-    def failure(self):
-        self.status = 1
-        self.notify_observers(IP_UPDATE_ERROR, "Service is failing")
-
-    def notfqdn(self):
-        self.status = 1
-        self.notify_observers(IP_UPDATE_ERROR, "The provided hostname '%s' is not a valid hostname!" % (self.hostname))
 
     def protocol(self):
         timeout = 60
@@ -65,28 +41,36 @@ class UpdateProtocol(Subject):
         log.debug("status %i, %s", r.status_code, r.text)
         if r.status_code == 200:
             if r.text.startswith("good "):
-                self.success()
+                self.notify_observers(IP_UPDATE_SUCCESS,
+                    "Updated IP address of '%s' to %s"
+                        % (self.hostname, self.theip))
                 return self.theip
             elif r.text.startswith('nochg'):
-                self.nochg()
                 return self.theip
             elif r.text == 'nohost':
-                self.nohost()
+                self.notify_observers(IP_UPDATE_ERROR,
+                    "Invalid/non-existant hostname: [%s]" % (self.hostname))
                 return 'nohost'
             elif r.text == 'abuse':
-                self.abuse()
+                self.notify_observers(IP_UPDATE_ERROR,
+                    "This client is considered to be abusive for hostname '%s'"
+                        % (self.hostname))
                 return 'abuse'
             elif r.text == '911':
-                self.failure()
+                self.notify_observers(IP_UPDATE_ERROR, "Service is failing")
                 return '911'
             elif r.text == 'notfqdn':
-                self.notfqdn()
+                self.notify_observers(IP_UPDATE_ERROR,
+                    "The provided hostname '%s' is not a valid hostname!"
+                        % (self.hostname))
                 return 'notfqdn'
             else:
-                self.status = 1
-                self.notify_observers(IP_UPDATE_ERROR, "Problem updating IP address of '%s' to %s: %s" % (self.hostname, self.theip, r.text))
+                self.notify_observers(IP_UPDATE_ERROR,
+                        "Problem updating IP address of '%s' to %s: %s"
+                            % (self.hostname, self.theip, r.text))
                 return r.text
         else:
-            self.status = 1
-            self.notify_observers(IP_UPDATE_ERROR, "Problem updating IP address of '%s' to %s: %s" % (self.hostname, self.theip, r.status_code))
+            self.notify_observers(IP_UPDATE_ERROR,
+                "Problem updating IP address of '%s' to %s: %s"
+                    % (self.hostname, self.theip, r.status_code))
             return 'invalid http status code: %s' % r.status_code
