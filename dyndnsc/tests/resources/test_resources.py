ef51866bbc97b7c39af619b3522893ce6b046bb4
Paul Kremer <pkremer@spurious.biz>
2014-12-14 22:19:07 +0100

    tests

--- dyndnsc/tests/resources/test_resources.py
+++ dyndnsc/tests/resources/test_resources.py
@@ -3,7 +3,7 @@
 import unittest
 import os
 
-from dyndnsc.resources import getFilename, PROFILES_INI
+from dyndnsc.resources import getFilename, getStream, getString, exists, PROFILES_INI
 
 
 class TestResources(unittest.TestCase):
@@ -14,5 +14,15 @@ class TestResources(unittest.TestCase):
     def tearDown(self):
         unittest.TestCase.tearDown(self)
 
-    def testGetFilename(self):
+    def test_get_filename(self):
         self.assertTrue(os.path.isfile(getFilename(PROFILES_INI)))
+
+    def test_exists(self):
+        self.assertTrue(exists(PROFILES_INI))
+        self.assertFalse(exists(getFilename(".fubar-non-existant")))
+
+    def test_get_string(self):
+        self.assertEqual(str, type(getString(PROFILES_INI)))
+
+    def test_get_stream(self):
+        self.assertTrue(hasattr(getStream(PROFILES_INI), 'close'))
