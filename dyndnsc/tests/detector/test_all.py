43a2bce246d0ea3c00bcdaeb8a8aa4ef8a70d2b6
Paul Kremer <pkremer@spurious.biz>
2015-02-14 18:01:49 +0100

    python2.6

--- dyndnsc/tests/detector/test_all.py
+++ dyndnsc/tests/detector/test_all.py
@@ -149,7 +149,9 @@ class TestIndividualDetectors(unittest.TestCase):
         self.assertEqual(None, detector.get_current_value())
         self.assertTrue(type(detector.detect()) in (type(None), str))
         # empty interface name must not fail construction
-        self.assertIsInstance(iface.IPDetector_Iface(iface=None), iface.IPDetector_Iface)
+        # broken in python2.6
+        # self.assertIsInstance(iface.IPDetector_Iface(iface=None), iface.IPDetector_Iface)
+        self.assertEqual(type(iface.IPDetector_Iface(iface=None)), iface.IPDetector_Iface)
         # invalid netmask must fail construction
         self.assertRaises(ValueError, iface.IPDetector_Iface, netmask='fubar')
         # unknown address family  must fail construction
