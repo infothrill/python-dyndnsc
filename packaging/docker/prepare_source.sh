8e6c645c7a7ac6e07dbda5ac78d5ed9bb8819c3d
Paul Kremer <pkremer@spurious.biz>
2018-04-16 22:18:10 +0200

    cwd back

--- packaging/docker/prepare_source.sh
+++ packaging/docker/prepare_source.sh
@@ -4,7 +4,7 @@
 # this script outputs the current version so we can tag the create image with it.
 if ! test -d src;
 then
-    cd ../../ && python setup.py -q sdist 1>&2
+    cd ../../ && python setup.py -q sdist 1>&2 && cd -
 	mkdir src
 	ls ../../dist/dyndnsc-*.tar.gz | xargs -n1 tar -C src --strip-components 1 -xzf
 	python -c "import re, os; print(re.compile(r'.*__version__ = \"(.*?)\"', re.S).match(open(os.path.join('src/dyndnsc', '__init__.py'), 'r').read()).group(1))" > src/version
