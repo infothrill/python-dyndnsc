677b76bdd8bc17992c6b0803d626dacc0e92f4c2
Paul Kremer <pkremer@spurious.biz>
2018-04-16 21:46:08 +0200

    Show pip output on stderr during docker image build

--- packaging/docker/prepare_source.sh
+++ packaging/docker/prepare_source.sh
@@ -4,7 +4,7 @@
 # this script outputs the current version so we can tag the create image with it.
 if ! test -d src;
 then
-	hash tox 2>&- || pip install tox > /dev/null
+	hash tox 2>&- || pip install tox 1>&2
 	tox -e build > /dev/null
 	mkdir src
 	ls ../../dist/*.tar.gz | grep dyndnsc | xargs -n1 tar -C src --strip-components 1 -xzf
