b9bb63ce4d87c6ce2c87690f11bbaed66213273b
Paul Kremer <pkremer@spurious.biz>
2018-04-16 22:10:08 +0200

    ok dockerhub, no tox

--- packaging/docker/prepare_source.sh
+++ packaging/docker/prepare_source.sh
@@ -4,8 +4,7 @@
 # this script outputs the current version so we can tag the create image with it.
 if ! test -d src;
 then
-	hash tox 2>&- || pip install -r <(pip freeze) --upgrade && pip install tox 1>&2
-	tox -e build 1>&2
+    python setup.py -q sdist 1>&2
 	mkdir src
 	ls ../../dist/dyndnsc-*.tar.gz | xargs -n1 tar -C src --strip-components 1 -xzf
 	python -c "import re, os; print(re.compile(r'.*__version__ = \"(.*?)\"', re.S).match(open(os.path.join('src/dyndnsc', '__init__.py'), 'r').read()).group(1))" > src/version
