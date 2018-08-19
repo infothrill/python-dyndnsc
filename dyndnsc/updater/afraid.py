11a3b5596987c3fe1af94d1e77a573aaf6f446d9
Paul Kremer <pkremer@spurious.biz>
2018-08-19 23:18:57 +0200

    avoid errors for S303

--- dyndnsc/updater/afraid.py
+++ dyndnsc/updater/afraid.py
@@ -69,9 +69,9 @@ def compute_auth_key(userid, password):
     """
     import sys
     if sys.version_info >= (3, 0):
-        return hashlib.sha1(b"|".join((userid.encode("ascii"),
+        return hashlib.sha1(b"|".join((userid.encode("ascii"),  # nosec
                                        password.encode("ascii")))).hexdigest()
-    return hashlib.sha1("|".join((userid, password))).hexdigest()
+    return hashlib.sha1("|".join((userid, password))).hexdigest()  # nosec
 
 
 def records(credentials, url="https://freedns.afraid.org/api/"):
