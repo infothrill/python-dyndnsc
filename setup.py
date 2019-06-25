faaced4af642dddd57b52f497492dc8f6e4eeb79
Paul Kremer <pkremer@spurious.biz>
2019-06-25 21:54:17 +0200

    python 3.7 support

--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ CLASSIFIERS = (
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6"
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7"
 )
 
 INSTALL_REQUIRES = [
